f3085c2a3211b58734daddd1c6c002f8ad2ebb6b
azalina <azalinahamid@gmail.com>
2018-07-25 11:56:36 +0800

    update register user form n controller

--- resources/views/frontend/register.blade.php
+++ resources/views/frontend/register.blade.php
@@ -6,40 +6,35 @@
     <section class="py-5">
       <div class="container">
        <h1>Register New User</h1><br>
-       @if($errors->any())
-        <div class="alert alert-danger">
-          <ul>
-            @foreach($errors->all() as $error)
-              <li>{{$error}}</li>
-            @endforeach
-          </ul>
-        </div>
-        @endif
+
+       @include('common.alert')
+       @include('common.form_error')
+
        <form method="POST" action="{{route('user.register.post')}}">
         @csrf
         
          <div class="form-group row">
            <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
            <div class="col-sm-10">
-             <input type="name" name="name" class="form-control" id="inputEmail3" placeholder="Name">
+             <input type="text" name="name" class="form-control" id="inputEmail3" placeholder="Name" value="{{old('name')}}">
            </div>
          </div>
          <div class="form-group row">
            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
            <div class="col-sm-10">
-             <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Email">
+             <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Email" value="{{old('email')}}">
            </div>
          </div>
          <div class="form-group row">
            <label for="inputEmail3" class="col-sm-2 col-form-label">IC No</label>
            <div class="col-sm-10">
-             <input type="ic" name="ic" class="form-control" id="inputEmail3" placeholder="IC No without dash (-)">
+             <input type="text" name="ic" class="form-control" id="inputEmail3" placeholder="IC No without dash (-)" value="{{old('ic')}}">
            </div>
          </div>
          <div class="form-group row">
            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
            <div class="col-sm-10">
-             <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
+             <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password" >
            </div>
          </div>
          <div class="form-group row">
